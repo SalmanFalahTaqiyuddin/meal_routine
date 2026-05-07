6d80e53ed3770689c5355e61956d77afff07277a
malikahroufghifari <malikah_ghifari_33rpl@student.smktelkom-mlg.sch.id>
2026-05-07 12:46:45 +0700

    mencoba

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -92,7 +92,7 @@ class _LoginScreenState extends State<LoginScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Spacer(flex: 3),
-              Text('Login', style: theme.textTheme.displayLarge),
+              Text('Malikah', style: theme.textTheme.displayLarge),
               const SizedBox(height: 36),
               TextField(
                 controller: _emailCtrl,
